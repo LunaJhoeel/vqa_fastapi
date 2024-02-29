086ab2b29b2f1be325e9cc987db4e1e1e16cf946
Jhoeel <lunajhoeel@gmail.com>
2024-02-28 23:38:49 -0500

    feat: Add Salesforce BLIP model from Hugging Face for inference

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 from typing import Annotated
 from fastapi import FastAPI, File, Form, UploadFile
+from transformers import BlipProcessor, BlipForQuestionAnswering
+from PIL import Image
+import io
 
 app = FastAPI()
+
+# Hugging Face test model
+processor = BlipProcessor.from_pretrained("Salesforce/blip-vqa-base")
+model = BlipForQuestionAnswering.from_pretrained("Salesforce/blip-vqa-base")
 
 
 @app.post("/visual-question/")
 async def visual_question(
         file: Annotated[UploadFile, File()],
         question: Annotated[str, Form()]
-):
+        ):
+    # Format the input image
+    image_data = await file.read()
+    image = Image.open(io.BytesIO(image_data)).convert('RGB')
+    
+    # Process the image and question
+    inputs = processor(image, question, return_tensors="pt")
+    
+    # Generate answer
+    out = model.generate(**inputs, max_new_tokens=30)
+    answer = processor.decode(out[0], skip_special_tokens=True)
+    
     return {
         "file_name": file.filename,
-        "question": question
-    }
+        "question": question,
+        "answer": answer
+        }
